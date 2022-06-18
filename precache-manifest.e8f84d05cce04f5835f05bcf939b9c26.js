3d73ac047f577f5d4a661924ef842dc0252d0d56
Sunder Kumar <sunderkumar2506@gmail.com>
2022-06-18 21:45:24 +0530

    Updates

---
+++ precache-manifest.e8f84d05cce04f5835f05bcf939b9c26.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "72ba7a89d13de7463c0f",
+    "url": "/kireiSenpai/static/css/main.fe344fd0.chunk.css"
+  },
+  {
+    "revision": "72ba7a89d13de7463c0f",
+    "url": "/kireiSenpai/static/js/main.72ba7a89.chunk.js"
+  },
+  {
+    "revision": "88b978cd6a0590648fe5",
+    "url": "/kireiSenpai/static/js/1.88b978cd.chunk.js"
+  },
+  {
+    "revision": "eba77a3dbc3ba6e96f6b",
+    "url": "/kireiSenpai/static/js/runtime~main.eba77a3d.js"
+  },
+  {
+    "revision": "a6c40d33f618bcbcf48443ff773b52e4",
+    "url": "/kireiSenpai/static/media/twitter.a6c40d33.svg"
+  },
+  {
+    "revision": "43e84724fe7e35f4f21b7ca1a80600b9",
+    "url": "/kireiSenpai/static/media/instagram.43e84724.svg"
+  },
+  {
+    "revision": "8b3b0e77a379ce56848571ba857405fb",
+    "url": "/kireiSenpai/static/media/facebook.8b3b0e77.svg"
+  },
+  {
+    "revision": "52290d6b6efdf68f0c1c45093fabcbfe",
+    "url": "/kireiSenpai/static/media/github.52290d6b.svg"
+  },
+  {
+    "revision": "287e585db40826422313dd072238d50b",
+    "url": "/kireiSenpai/static/media/dp.287e585d.jpg"
+  },
+  {
+    "revision": "1af69e7ec473aab22dca9168385d0138",
+    "url": "/kireiSenpai/index.html"
+  }
+];
